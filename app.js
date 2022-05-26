d5d5da97f4408f799733879e013d3e50cbdeb483
jcmonnet <jcmonnet0408@gmail.com>
2022-05-26 17:50:58 +0200

    ajout suppression contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // cibler élements du DOM
-let aside=document.querySelector('.main_article_aside');
-let contacts=[];
+let aside = document.querySelector('.main_article_aside');
+let contacts = [];
 
 /**
  * 
@@ -8,7 +8,7 @@ let contacts=[];
  * @param {string} nom nom du contact
  * @returns {object} objet du contact
  */
-function creerContact(prenom,nom) {
+function creerContact(prenom, nom) {
     return {
         prenom: prenom,
         nom: nom,
@@ -19,26 +19,39 @@ function creerContact(prenom,nom) {
 }
 
 // ajouter contact au tableau contactS
-let contact= creerContact('Johnny', 'begood');
+let contact = creerContact('Johnny', 'begood');
 contacts.push(contact);
 // ou 
-contacts.push(creerContact('luke','skywalker'));
+contacts.push(creerContact('luke', 'skywalker'));
 // ou
-contacts = [...contacts,creerContact('dark','vador')];
-contacts = [...contacts,creerContact('dark','vador 2')];
-contacts = [...contacts,creerContact('dark','vador 3')];
-contacts = [...contacts,creerContact('dark','vador 4')];
-contacts = [...contacts,creerContact('dark','vador 5')];
-contacts = [...contacts,creerContact('dark','vador 6')];
+contacts = [...contacts, creerContact('dark', 'vador')];
+contacts = [...contacts, creerContact('dark', 'vador 2')];
+contacts = [...contacts, creerContact('dark', 'vador 3')];
+contacts = [...contacts, creerContact('dark', 'vador 4')];
+contacts = [...contacts, creerContact('dark', 'vador 5')];
+contacts = [...contacts, creerContact('dark', 'vador 6')];
 
 function afficheContact() {
-    aside.innerHTML = ''; 
-    contacts.forEach((contact)=> {
+    aside.innerHTML = '';
+    contacts.forEach((contact, index) => {
         let div = document.createElement('div');
         div.classList.add('main_article_aside_card');
         div.innerHTML = `<p>${contact.getNomComplet()}</p>`;
-        div.innerHTML += `<p class="corbeille"></p>`
+        div.innerHTML += `<p class="corbeille" id=${index}>&#9851;</p>`
         aside.appendChild(div);
     })
+    suppressionContact();
+}
+
+function suppressionContact() {
+    let corbeilles = document.querySelectorAll('.corbeille');
+    corbeilles.forEach((corbeille) => {
+        corbeille.addEventListener('click', function (evenement) {
+            contacts.splice(evenement.target.id, 1);
+            afficheContact();
+        });
+    });
 }
 afficheContact();
+
+
